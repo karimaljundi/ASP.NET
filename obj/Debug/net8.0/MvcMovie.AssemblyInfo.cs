c30a96955d11af555f870474c0d52b610743085c
karim <jundiak@gmail.com>
2024-05-28 02:43:23 -0400

    using sqlite as the db

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa91b5ca5c70951ce2695f7ba4e6995f8d8131f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431a8396ad1336307feca76f5c901985749f14fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
